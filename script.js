d5d337a68177b2322b23771c22188b3850a6b87b
Anuragkolhe <128228032+Anuragkolhe@users.noreply.github.com>
2023-05-24 17:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getFormvalue() {
   const fname = document.getElementById("fname").value;
   const lname = document.getElementById("lname").value;
-  alert("First name: " + fname + "\nLast name: " + lname);
+  alert( fname + lname);
 }
